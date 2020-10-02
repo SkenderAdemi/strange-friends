d4f623280310e461ab1070d8d1ded051b3d501f6
Skender Ademi <skender@giggle.tips>
2020-10-02 15:47:07 +0200

    Updates

---
+++ precache-manifest.87da5c8b18dc5e8bc6cd4475b7e0733d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd313619ff8fd3e5eed4a4618c4bfa7",
+    "url": "/strange-friends/index.html"
+  },
+  {
+    "revision": "5a88a4a473c90275c71a",
+    "url": "/strange-friends/static/css/main.1e78ef83.chunk.css"
+  },
+  {
+    "revision": "83e4207d298c70050087",
+    "url": "/strange-friends/static/js/2.e5d10b06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/strange-friends/static/js/2.e5d10b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a88a4a473c90275c71a",
+    "url": "/strange-friends/static/js/main.13190d2a.chunk.js"
+  },
+  {
+    "revision": "a7372c54f9a459e54d6a",
+    "url": "/strange-friends/static/js/runtime-main.d642fd99.js"
+  }
+]);
